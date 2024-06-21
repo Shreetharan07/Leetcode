6073dfaff15c73942e750278a56cfaf6bcade7ac
Shreetharan07 <118371414+Shreetharan07@users.noreply.github.com>
2024-06-21 11:16:52 +0530

    Time: 5 ms (19.74%) | Memory: 42 MB (56.09%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
@@ -10,18 +10,6 @@ class Solution {
         if(li.size()<2) return -1;
         Collections.sort(li);
         return li.get(li.size()-2);
-        /*
-        for(int i=0;i<s.length();i++){
-            if(Character.isDigit(s.charAt(i))){
-                arr.add((int)s.charAt(i));
-            }
-        }
-        Set<Integer> li=new HashSet<>(arr);
-        ArrayList<Integer> arr1=new ArrayList<>(li);
-        Collections.sort(arr1);
-        int l=arr1.size();
-        if(l==1) return -1;
-        return arr1.get(l-2);
-        */
+        
     }
 }
