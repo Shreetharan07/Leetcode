ae66d093aa0c7aa5da1ebdb7f56903424b6f5ad2
Shreetharan07 <118371414+Shreetharan07@users.noreply.github.com>
2024-07-06 15:54:00 +0530

    Time: 1974 ms (5.94%) | Memory: 59.7 MB (5.25%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -9,17 +9,17 @@ class Solution {
         for (int i = 0; i < n - 2; i++) {
             if (nums[i] == 1) continue;
 
-            // Toggle current element and next two elements
+           
             nums[i] = 1 - nums[i];
             nums[i + 1] = 1 - nums[i + 1];
             nums[i + 2] = 1 - nums[i + 2];
             count++;
 
-            // Check if the array is now equal to the target array
+            
             if (Arrays.equals(nums, arr)) return count;
         }
 
-        // Final check outside the loop if we haven't returned yet
+      
         if (Arrays.equals(nums, arr)) return count;
         return -1;
     }
